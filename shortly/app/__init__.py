7b65d5ddef2cb6522a539e24974faf1bb7a2ee13
Bartek Krzys <bartek@mergedup.com>
2022-11-12 12:44:15 +0100

    ✅ Update tests to have 100% coverage.

--- shortly/app/__init__.py
+++ shortly/app/__init__.py
@@ -1,1 +1,1 @@
-from .main import app  # noqa
+from .main import app
--- tests/unit/test_example.py
+++ tests/unit/test_example.py
@@ -1,5 +1,7 @@
 from shortly.app import app
+from shortly.app.config import Settings
 
 
 def test_example():
     assert app
+    assert Settings()
