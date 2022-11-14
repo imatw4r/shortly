3d0e873361c18ed31f834073aadd06cfb92edb7e
Przemysław Różycki <81198593+trazola@users.noreply.github.com>
2022-11-14 22:24:30 +0100

    Add alembic to migration and refactor project structure (#6)
    * Add mypy config
    
    * Add alembic package
    
    * Add py.typed
    
    * Add alembic configuration
    
    * Add base model
    
    * Fix black issues
    
    * Add empty line to end file
    
    * Fix project stucture

--- shortly/app/adapters/sqlalchemy/__init__.py
+++ shortly/app/adapters/sqlalchemy/__init__.py
@@ -3,14 +3,16 @@ from sqlalchemy.orm import sessionmaker
 
 from shortly.app.config import settings
 
-SQLALCHEMY_ENGINE = create_engine(
+from .base import metadata
+
+ENGINE = create_engine(
     url=settings.db.get_async_uri().get_secret_value(),
     pool_pre_ping=True,
     echo=settings.db.ECHO_LOGS,
 )
 
-SQLALCHEMY_SESSION_FACTORY = sessionmaker(
-    bind=SQLALCHEMY_ENGINE,
+SESSION_FACTORY = sessionmaker(
+    bind=ENGINE,
     autoflush=False,
     autocommit=False,
     expire_on_commit=False,
