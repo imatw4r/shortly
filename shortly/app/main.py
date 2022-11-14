ee2d3ff18f81a80b321be2929e002b6e762bbafd
trazola <przemyslaw.rozycki1996@gmail.com>
2022-11-14 22:54:52 +0100

    Add profiler middleware

--- shortly/app/main.py
+++ shortly/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+from shortly.app.middlewares import setup_middlewares
 from shortly.app.signals import setup_signals
 
 app = FastAPI()
 
 setup_signals(app=app)
+setup_middlewares(app=app)
