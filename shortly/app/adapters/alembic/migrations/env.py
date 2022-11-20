726ed5f65f538abf30550ade2299ee280c88701f
trazola <przemyslaw.rozycki1996@gmail.com>
2022-11-20 15:37:55 +0100

    Add migration job

--- shortly/app/adapters/alembic/migrations/env.py
+++ shortly/app/adapters/alembic/migrations/env.py
@@ -29,15 +29,12 @@ target_metadata = metadata
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
-
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
     we don't even need a DBAPI to be available.
-
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
     url = settings.db.get_sync_uri()
     context.configure(
@@ -54,10 +51,8 @@ def run_migrations_offline() -> None:
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
-
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
     configuration = config.get_section(name=config.config_ini_section)
     url = settings.db.get_sync_uri()
